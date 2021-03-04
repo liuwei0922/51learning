f1396401b893389b8e0d13fc7d04175e72f080ac
liuwei0922 <qinmoxiao@qq.com>
2021-03-04 22:34:47 +0800

    LED点阵显示字母LIU

--- LED点阵亮灯/main.c
+++ LED点阵亮灯/main.c
@@ -1,8 +1,47 @@
 #include <8051.h>
 #include "hc595.h"
-#define LED P0_0
+#define LED P0
+#define switchs P1_0
+unsigned char __code LEDBUFF[]={0xf8,0xfa,0xc2,0xea,0x0a,0x6f,0x47,0x7f};
+
 
 void main(){
-    LED = 0;
-    HC595(0b10000000);
+    // 打开总中断
+    EA = 1;
+    // 定时中断
+    ET0 = 1;
+    // 定时模式
+    TMOD = 0X01;
+    // 定时大小为1ms
+    TH0 = 0xFC;
+	TL0 = 0x67;
+    // 打开定时器
+    TR0 = 1;
+    // P1低电位
+    switchs = 0;
+    // 全灭
+    LED = 0Xff;
+    //unsigned char i =0;
+    while (1);
+    
+    //HC595(0b10000000);
+}
+void InterruputTime0() __interrupt 1 {
+    static unsigned char  times = 0 ;
+    static unsigned char hcvar = 0b00000001;
+    TH0 = 0xFC;
+	TL0 = 0x67;
+    // 不要闪烁
+    LED = 0XFF;
+    // 竖坐标亮
+    HC595(hcvar); 
+    hcvar <<= 1; 
+    // 横坐标亮
+    LED = LEDBUFF[times];
+    times ++ ;
+    if(times >=8){
+        times = 0; 
+        hcvar = 1;
+    }
+           
 }
