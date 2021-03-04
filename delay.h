5fd3ee0e7968ba5cc38130b6f45536f1bd34e88c
liuwei0922 <qinmoxiao@qq.com>
2021-03-04 08:08:55 +0800

    延时

--- delay.h
+++ delay.h
@@ -1,20 +1,5 @@
 #ifndef _DELAY_
 #define	_DELAY_
+extern void delay(unsigned char times);
+#endif 
 
-void delay(unsigned char times){
-	unsigned char mnt = 0;
-	TMOD =0x01;
-	TH0 = 0xFC;	 
-	TL0 = 0x67;
-	TR0 = 1; 
-	/*
-	while (mnt<=times){
-		if(TF0==1){
-			TF0=0;
-			TH0 = 0xFC;
-			TL0 = 0x67;
-			mnt++;
-		}
-	}
-	*/
-}
