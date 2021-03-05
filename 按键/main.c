025dc727467185dc39e2d256ee37c80f3184c5ac
liuwei0922 <qinmoxiao@qq.com>
2021-03-05 15:24:22 +0800

    按键可以按一下改变一次灯的状态了

--- 按键/main.c
+++ 按键/main.c
@@ -4,6 +4,11 @@
 #define K3 P3_2
 #define K4 P3_3
 #define LED P2_0
+
+void delay(unsigned int i){
+    while (i--);
+    
+}
 void main (){
     // 按键赋初值
     K1 = 1;
@@ -11,17 +16,21 @@ void main (){
     K3 = 1;
     K4 = 1;
 
-    LED = 1;
+    unsigned char  led_backup = 0 ;
+    __bit backup  = 1;
+    
+    LED = 0b01;
+    
     while (1)
-    {
-        if(K4==0){
-            LED = 0;
-        }
-        if (K4 == 1)
+    {   
+        if (K4 != backup)
         {
-            LED = 1;
+            LED = led_backup ;
+            led_backup = ~ led_backup;
+            delay(20000);
+            backup = K4;
         }
-        
+           
     }
     
 }
