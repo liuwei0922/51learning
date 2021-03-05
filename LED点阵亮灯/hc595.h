9155ac5c5e7ec1e618680a2daf05cea6dbf04ca3
liuwei0922 <qinmoxiao@qq.com>
2021-03-05 19:35:36 +0800

    整理所有的头文件

--- LED点阵亮灯/hc595.h
+++
@@ -1,5 +0,0 @@
-#ifndef _HC595_
-#define _HC595_
-
-extern void HC595(unsigned char _data_);
-#endif
--- 数码管动态显示_24小时/delay.h
+++
@@ -1,5 +0,0 @@
-#ifndef _DELAY_
-#define	_DELAY_
-extern void delay(unsigned char times);
-#endif 
-
