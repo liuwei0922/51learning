c489cce08a2384c86c6c22ab3668dbd4c85604b6
liuwei0922 <qinmoxiao@qq.com>
2021-03-07 17:11:52 +0800

    按键控制

---
+++ include/keysdriver.h
@@ -0,0 +1,8 @@
+#ifndef _KEY_DRIVER_
+#define _KEY_DRIVER_
+
+extern void keys_driver(unsigned char  number_to_out[]);
+extern void keys_action(unsigned char key_code,unsigned char number_to_out[]);
+extern void keys_scan();
+#endif 
+
