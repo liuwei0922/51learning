a5cd901b138d9e23d17d530a42e1820167f71fc0
liuwei0922 <qinmoxiao@qq.com>
2021-03-09 14:00:57 +0800

    添加一个时间中断的函数

--- src/delay.c
+++ src/delay.c
@@ -1,4 +1,6 @@
 #include <8051.h>
+unsigned char T0RH = 0;
+unsigned char T0RL = 0;
 void delay(unsigned char times){
 	//unsigned char mnt = 0;
 	TMOD =0x01;
@@ -15,4 +17,18 @@ void delay(unsigned char times){
 		}
 	}
 	*/
+}
+void config_time_0(unsigned char ms){
+	unsigned long tmp;
+	tmp = 11059200 / 12; //计时器频率
+	tmp = (tmp*ms)/1000; //计时器计数值
+	tmp = 65536 - tmp; //计时器重载值
+	T0RH = (char)(tmp>>8);//高位 
+	T0RL = (char)(tmp);//低位
+	TMOD &= 0XF0;//定时模式
+	TMOD |= 0X01;
+	TH0 = T0RH;
+	TL0 = T0RL;
+	ET0 = 1;
+	TR0 = 1;
 }
