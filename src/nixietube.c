8c07404889d2690f948f646ce2e65f3f684c980d
liuwei0922 <qinmoxiao@qq.com>
2021-03-09 15:46:44 +0800

    数码管显示函数更改

--- src/nixietube.c
+++ src/nixietube.c
@@ -4,66 +4,23 @@
 #define ADD2  P2_4
 
 void tube (unsigned char led) {
-	 P0 =  ~ 0xFF;
-	 switch (led)
-	 {
-		case 0:
-		 	ADD0 = 0;
-			ADD1 = 0;
-			ADD2 = 0;
-			break;
-		case 1: 
-			ADD0 = 1;
-			ADD1 = 0;
-			ADD2 = 0;
-			break;
-		case 2: 
-			ADD0=0;	
-			ADD1=1;	
-			ADD2=0;
-			break;
-		case 3: 
-			ADD0=1;
-			ADD1=1;
-			ADD2=0;	
-			break;
-		case 4: 
-			ADD0=0;
-			ADD1=0;	
-			ADD2=1;
-			break;
-		case 5: 
-			ADD0=1;
-			ADD1=0;
-			ADD2=1;
-			break;
-		case 6: 			
-			ADD0=0;	
-			ADD1=1;	
-			ADD2=1;
-			break;
-		case 7:
-			ADD0=1;	
-			ADD1=1;
-			ADD2=1;
-			break;
-		default: break;
-		}
+	P0 =  ~ 0xFF;
+	P2 &= 0xf1|(led<<1);
 }
 void lighting(unsigned char num){
 	P0 =  ~ 0xFF;
   	unsigned char __code  Led[]={0xC0, 0xF9, 0xA4, 0xB0, 0x99, 0x92, 0x82, 0xF8,0x80,0x90,0xff};
 	P0 = ~ Led[num];
 }
-void number_for_show(unsigned long number,unsigned char number_to_out[]){
+void number_for_show(unsigned long number,unsigned char number_to_out[],unsigned char length){
     signed char i;
     unsigned char num_buff[6];
-    for ( i = 0; i < 7 ; i++)
+    for ( i = 0; i < length ; i++)
     {
         num_buff[i] = number%10;
         number /= 10;
     }
-    for ( i = 6; i>=1; i--)
+    for ( i = length -1; i>=1; i--)
     {
         if (num_buff[i] == 0)
         {
