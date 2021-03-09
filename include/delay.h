4e345c6f7e04db7f092a76b20c2eb3d01d1fbbeb
liuwei0922 <qinmoxiao@qq.com>
2021-03-09 14:00:35 +0800

    添加一个设置时间中断的函数

--- include/delay.h
+++ include/delay.h
@@ -1,5 +1,8 @@
-#ifndef _DELAY_
-#define	_DELAY_
+#ifndef _DELAY_ 
+#define	_DELAY_ 
+extern unsigned char T0RH ;
+extern unsigned char T0RL ;
 extern void delay(unsigned char times);
+extern void config_time_0(unsigned char ms);
 #endif 
 
