2bf8d442942e277bbbfbb11ddf711265fa567eb9
liuwei0922 <qinmoxiao@qq.com>
2021-03-09 16:30:36 +0800

    增加一个定时器

--- include/delay.h
+++ include/delay.h
@@ -4,5 +4,6 @@ extern unsigned char T0RH ;
 extern unsigned char T0RL ;
 extern void delay(unsigned char times);
 extern void config_time_0(unsigned char ms);
+extern void config_time_1(unsigned char ms);
 #endif 
 
