1e1ad064ec2dc8e7f2b461f8099e6047f793e79f
liuwei0922 <qinmoxiao@qq.com>
2021-03-11 15:19:55 +0800

    串口通信模块

--- 样例:串口通信/main.c
+++ 样例:串口通信/main.c
@@ -1,91 +1,29 @@
 #include <8051.h>
-#include "../include/delay.h"
-#define pin_rxd P3_0
-#define pin_txd P3_1
-__bit rxd_or_txd = 0;
-__bit rxd_end = 0;
-__bit txd_end = 0;
-unsigned char txd_buff = 0;
-unsigned char rxd_buff = 0;
-void start_rxd();
-void start_txd(unsigned char dat);
-void main (){
+#define _nop_() __asm NOP __endasm
+void config_communication(unsigned int baud );
+void main(){
     EA = 1;
-    mod2_time_0(38400);
-    while (1)
-    {
-        while (pin_rxd);
-        start_rxd();
-        while(!rxd_end);
-        start_txd(rxd_buff);
-        while(!txd_end);
+    config_communication(600);
+    while(1);
+}
+void interrupt_urt () __interrupt 4{
+    if(RI){
+        RI = 0;
+        SBUF = SBUF+1;
+    }
+    if(TI){
+        TI = 0;
     }
     
 }
-void interrupt_time_0 () __interrupt 1{
-    static unsigned char count = 0;
-    if(rxd_or_txd)
-    {
-        count++;
-        if (count <= 8)
-        {
-            pin_txd = txd_buff & 0x01;
-            txd_buff >>= 1;
-        }else if (count == 9)
-        {
-            pin_txd = 1;
-        }else{
-            count = 0;
-            TR0 = 0;
-            txd_end = 1;
-        }
-    }
-    else{
-        if (count == 0 )
-        {
-            if (!pin_rxd)
-            {
-                rxd_buff = 0;
-                count ++;
-            }
-            else{
-                TR0 = 0;
-            }
-        }
-        else if (count <= 8)
-        {
-            rxd_buff >>1;
-            //rxd_buff = rxd_buff|(pin_rxd<<7);
-            if (pin_rxd)
-            {
-                rxd_buff |= 0x80;
-            }
-            count++;
-        }else{
-            count = 0;
-            TR0 =0;
-            if (pin_rxd)
-            {
-                rxd_end = 1;
-            }
-            
-        }
-    }
-}
-void start_rxd(){
-    TL0 = 256 - ((256 - TH0 )>>1);
-    ET0 = 1;
-    TR0 = 1;
-    rxd_end = 0;
-    rxd_or_txd = 0;
-
-}
-void start_txd(unsigned char dat){
-    txd_buff = dat;
-    TL0 = TH0 ;
-    ET0 = 1;
-    TR0 = 1;
-    pin_txd = 0;
-    txd_end = 0;
-    rxd_or_txd = 1;
+void config_communication(unsigned int baud){
+    SCON = 0X50;
+    TMOD &= 0X0F;
+    TMOD |= 0X20;
+    TH1 = 256 - (12010000/12/32/baud);
+    TL1 = TH1;
+    //PCON = 0X80;
+    ET1 = 0;
+    TR1 = 1;
+    ES = 1;
 }
